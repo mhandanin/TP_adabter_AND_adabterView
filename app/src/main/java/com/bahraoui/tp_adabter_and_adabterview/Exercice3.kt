7f53b29f1231b89a0c7bb9e0c3eb268102549d9d
mhandanin <mhandanin@gmail.com>
2024-11-08 12:54:52 +0100

    Exercice 3

--- app/src/main/java/com/bahraoui/tp_adabter_and_adabterview/Exercice3.kt
+++ app/src/main/java/com/bahraoui/tp_adabter_and_adabterview/Exercice3.kt
@@ -10,8 +10,23 @@ class Exercice3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        enableEdgeToEdge()
-        setContentView(R.layout.layout_exercice2)
-        val lv=findViewById<ListView>(R.id.lv)
+        setContentView(R.layout.layout_exercice3)
+        val lv=findViewById<ListView>(R.id.affichage)
 
+        val listPays = listOf(
+            mapOf("pays" to "Palestine", "monnaie" to "Pound", "iso" to "PP"),
+            mapOf("pays" to "Algérie", "monnaie" to "Dinar algérien", "iso" to "DZD"),
+            mapOf("pays" to "Afghanistan", "monnaie" to "Afghani", "iso" to "AFN"),
+            mapOf("pays" to "Arabie Saoudite", "monnaie" to "Riyal saoudien", "iso" to "SAR"),
+            mapOf("pays" to "Belgique", "monnaie" to "Euro", "iso" to "EUR"),
+            mapOf("pays" to "Bénin", "monnaie" to "Franc CFA", "iso" to "XOF"),
+            mapOf("pays" to "Brésil", "monnaie" to "Réal brésilien", "iso" to "BRL"),
+            mapOf("pays" to "Côte d'Ivoire", "monnaie" to "Franc CFA", "iso" to "XOF")
+        )
+
+
+        val adapter=SimpleAdapter(this,listPays,R.layout.customadabter_exercice3, arrayOf("pays","monnaie","iso"),
+            intArrayOf(R.id.nomPays,R.id.monnaie,R.id.iso))
+        lv.adapter=adapter
     }
 }
